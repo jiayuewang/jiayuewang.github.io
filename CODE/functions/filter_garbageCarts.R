a5346dbc3428ca73b17c821abc4931092f088007
jiayue wang <jiayue3@illinois.edu>
2019-01-17 22:25:34 -0600

    delete

--- CODE/functions/filter_garbageCarts.R
+++
@@ -1,5 +0,0 @@
-filter_garbageCarts <- function(garbageCarts){
-    garbageCarts <- garbageCarts[!is.na(Latitude) & !is.na(Longitude) & !is.na(Creation_Date)]
-    garbageCarts <- garbageCarts[Status %in% c("Completed", "Open")]
-    garbageCarts
-}
--- CODE/functions/filter_sanitationComplaints.R
+++
@@ -1,5 +0,0 @@
-filter_sanitationComplaints <- function(sanitationComplaints) {
-    sanitationComplaints <- sanitationComplaints[!is.na(Latitude) & !is.na(Longitude) & !is.na(Creation_Date)]
-    sanitationComplaints <- sanitationComplaints[Status %in% c("Completed", "Open")]
-    sanitationComplaints
-}
